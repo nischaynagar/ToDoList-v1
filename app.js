9217445f9a2929f6acd54ace36c408aca4c61769
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2020-10-05 03:32:40 +0530

    added gitignore file and setup heroku

--- app.js
+++ app.js
@@ -150,6 +150,12 @@ app.post("/work", function (req, res) {
 	res.redirect("/work");
 });
 
-app.listen(3000, function () {
-	console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3000;
+}
+
+
+app.listen(port, function () {
+	console.log("Server has started on Successfully");
 });
